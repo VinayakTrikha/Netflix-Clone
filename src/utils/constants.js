e23240e5c3cb8e4f4c87fcb38902eee99f0d88a8
Vinayak Trikha <vinayaktrikha@izooto.com>
2025-09-02 18:26:17 +0530

    cloudflare support added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,7 +13,10 @@ export const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.REACT_APP_TMDB_KEY}`,
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
   },
 };
+
+console.log(import.meta.env.VITE_TMDB_KEY);
+
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
